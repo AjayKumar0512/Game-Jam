0f3d4103fbb8ab8417304bf3e6819df8bd6ae63d
Ajay Kumar <ajay.kumar@splashlearn.com>
2022-11-14 11:44:36 +0530

    dummy health game created

--- circuit game/src/StringConstants.js
+++
@@ -1,9 +0,0 @@
-const StringConstants = {
-    ACTIVITY_NAME: "toy-builder",
-
-    MIX_PANEL_EVENT: {
-        OPENED_ACTIVITY: "Opened Activity",
-        ACTIVITY_INTERACTED: "Activity Interacted",
-        CLOSED_ACTIVITY: "Closed Activity"
-    }
-};
